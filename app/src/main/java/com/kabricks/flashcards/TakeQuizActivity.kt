062f52ad86f7b2377daeda7c1d2a8a9d08d136da
Kabricks <kabuyedouglas53@gmail.com>
2021-03-18 20:12:10 +0300

    add new takequiz activity

---
+++ app/src/main/java/com/kabricks/flashcards/TakeQuizActivity.kt
@@ -0,0 +1,11 @@
+package com.kabricks.flashcards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TakeQuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_take_quiz)
+    }
+}
